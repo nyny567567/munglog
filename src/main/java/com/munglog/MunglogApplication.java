9f66553c6ac57fd95e52159ad2d82a27851fcda3
nyny567567 <ny567@naver.com>
2026-06-14 18:29:44 +0900

    chore: Spring Boot 초기 프로젝트 세팅
    - Java 17, Gradle 기반 프로젝트 생성
    
    - 필수 의존성 추가 (Spring Web, Lombok, Data JPA, MySQL)
    
    - 설정 파일 변경 (properties -> yml)
    
    - 기존 기획 문서(docs)와 백엔드 코드 단일 레포지토리로 통합

---
+++ src/main/java/com/munglog/MunglogApplication.java
@@ -0,0 +1,13 @@
+package com.munglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunglogApplication.class, args);
+	}
+
+}
