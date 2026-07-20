3ea221c94ef92ac257ef2628068be7e6fe38f9e2
nyny567567 <ny567@naver.com>
2026-07-20 18:59:53 +0900

    feat: 회원가입 처리를 위한 Member 엔티티 기본 구조 설계

---
+++ src/main/java/com/munglog/entity/Member.java
@@ -0,0 +1,33 @@
+package com.munglog.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
